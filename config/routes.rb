e9d2d8ac7d91cd3faf10ee094ed76d8a83d72dfa
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-05-25 16:57:33 +0200

    AddedControllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'positions/new'
+  get 'positions/create'
+  get 'positions/destroy'
+  get 'portfolios/index'
+  get 'portfolios/show'
+  get 'portfolios/create'
+  get 'portfolios/update'
+  get 'portfolios/destroy'
+  get 'infos/index'
+  get 'infos/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

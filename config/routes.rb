c17de0349aeb16c65fb08b3b4613d3ed4ce293db
gwendal-dev <78210637+gwendal-dev@users.noreply.github.com>
2021-05-26 10:08:21 +0200

    market

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'portfolio/index'
+  get 'portfolio/show'
+  get 'portfolio/edit'
+  get 'portfolio/new'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :market, only: :index
+  resources :portfolio
 end

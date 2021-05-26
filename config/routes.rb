f6108e1eab3dd24b96d2d7392e796e9e0ca1369d
Charlotte Roederer <lemaildecharlotteroederer@gmail.com>
2021-05-26 10:59:42 +0200

    controller portfolio et routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  get 'positions/new'
-  get 'positions/create'
-  get 'positions/destroy'
-  get 'portfolios/index'
-  get 'portfolios/show'
-  get 'portfolios/create'
-  get 'portfolios/update'
-  get 'portfolios/destroy'
-  get 'infos/index'
-  get 'infos/show'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :market, only: :index
+  resources :portfolios, only: [:index, :show, :create, :update, :destroy] do
+    resources :positions, only: [:new, :create, :destroy]
+  end
+  resources :infos, only: [:index, :show]
 end

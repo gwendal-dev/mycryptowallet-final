efd6de658bfc472f22d3a17f532b383321551997
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-05-26 12:46:30 +0200

    infos#index OP

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,8 +1,23 @@
+require 'json'
+require 'net/http'
+
 class InfosController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   def index
+    @data = get_API
   end
 
   def show
+  end
+
+  private
+
+  def get_API
+
+    url = 'https://api.lunarcrush.com/v2?data=feeds&sources=news&key=bju9urccdhffjgowh16was'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    data =JSON.parse(response)
+
   end
 end

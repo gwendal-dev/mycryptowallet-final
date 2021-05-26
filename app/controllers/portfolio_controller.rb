6e9a568304f24de663dbba1c8912ae364bd8dba6
gwendal-dev <78210637+gwendal-dev@users.noreply.github.com>
2021-05-26 10:57:19 +0200

    show

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,14 +1,14 @@
 class PortfolioController < ApplicationController
   def index
+  end
+
+  def show
     @coins = Coin.all
     @total = 0
     @positions = Position.all
     @positions.each do |position|
       @total += position.amount
     end
-  end
-
-  def show
   end
 
   def edit

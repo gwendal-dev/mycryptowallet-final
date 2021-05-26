1ac136f479f377af98925d674120c45040378908
gwendal-dev <78210637+gwendal-dev@users.noreply.github.com>
2021-05-26 14:29:16 +0200

    position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,6 +5,16 @@ class PortfoliosController < ApplicationController
 
   def show
     @portfolio = Portfolio.find(params[:id])
+    @coins = Coin.all
+    @total = 0
+    @positions = Position.all
+    @positions.each do |position|
+      @coins.each do |coin|
+        if position.coin == coin
+          @total += position.quantity * coin.price
+        end
+      end
+    end
   end
 
   def create

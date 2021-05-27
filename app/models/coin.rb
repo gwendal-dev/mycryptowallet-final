6bbfec94c025a15c757af8b068552af7dda9b24e
gwendal-dev <78210637+gwendal-dev@users.noreply.github.com>
2021-05-27 17:37:27 +0200

    card

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -2,4 +2,8 @@ class Coin < ApplicationRecord
   has_many :positions
   validates :title, presence: true, uniqueness: true
   validates :price, presence: true
+
+  def quantity
+    self.positions&.map(&:quantity).sum
+  end
 end

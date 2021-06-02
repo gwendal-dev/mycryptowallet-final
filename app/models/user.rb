1480513a4ce81f5a1bc85e97d023b2ff8178796c
Charlotte Roederer <lemaildecharlotteroederer@gmail.com>
2021-06-02 12:19:59 +0200

    à supprimer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :portfolios
-  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
@@ -10,7 +9,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   has_many :room_messages,
            dependent: :destroy
-
   def gravatar_url
     gravatar_id = Digest::MD5::hexdigest(email).downcase
     "https://gravatar.com/avatar/#{gravatar_id}.png"

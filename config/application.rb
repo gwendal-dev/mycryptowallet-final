8e5fbf9edcbda0f63ef72956440fc66b8b165525
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-06-03 12:05:53 +0200

    precompile-corrected

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module MycryptowalletFinal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.action_mailer.default_url_options = { host: "localhost:3000" }
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

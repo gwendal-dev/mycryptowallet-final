45f13c2263889c60d73f7736f4f5f569c3f636ad
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-06-03 11:07:12 +0200

    config.hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,10 +5,13 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.hosts << "mycryptowallet.herokuapp.com"
   config.cache_classes = false
   config.assets.compile = true
   # Do not eager load code on boot.
   config.eager_load = false
+
+  config.hosts << /[a-z0-9]+\.c9users\.io/
 
   # Show full error reports.
   config.consider_all_requests_local = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,10 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+
   config.cache_classes = true
+
+  config.hosts << "mycryptowallet.herokuapp.com"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -22,6 +25,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass

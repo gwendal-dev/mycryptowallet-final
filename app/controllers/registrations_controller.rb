f1b7feed95953922a600c592a2d9213e035a23bd
gwendal-dev <78210637+gwendal-dev@users.noreply.github.com>
2021-06-07 19:58:27 +0200

    bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
-  def create
-    if verify_recaptcha
-      super
-    else
-      build_resource(sign_up_params)
-      clean_up_passwords(resource)
-      flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
-      flash.delete :recaptcha_error
+  prepend_before_action :check_captcha, only: [:create] # Change this to be any actions you want to protect.
+
+  private
+
+  def check_captcha
+    return if !verify_recaptcha # verify_recaptcha(action: 'signup') for v3
+
+    self.resource = resource_class.new sign_up_params
+    resource.validate # Look for any other validation errors besides reCAPTCHA
+    set_minimum_password_length
+
+    respond_with_navigational(resource) do
+      flash.discard(:recaptcha_error) # We need to discard flash to avoid showing it on the next page reload
       render :new
     end
   end
